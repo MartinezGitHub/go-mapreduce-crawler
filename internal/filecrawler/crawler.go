9a3e5516f125fcc11bd49ab0222ae9c86fef92a3
Fedorov_Maksim <409754@edu.itmo.ru>
2024-11-29 02:50:17 +0300

    1 attempt

--- internal/filecrawler/crawler.go
+++ internal/filecrawler/crawler.go
@@ -4,6 +4,10 @@ import (
 	"context"
 	"crawler/internal/fs"
 	"crawler/internal/workerpool"
+	"encoding/json"
+	"fmt"
+	"log"
+	"sync"
 )
 
 // Configuration holds the configuration for the crawler, specifying the number of workers for
@@ -33,7 +37,7 @@ type Combiner[R any] func(current R, accum R) R
 // simultaneously.
 type Crawler[T, R any] interface {
 	// Collect performs the full crawling operation, coordinating with the file system
-	// and worker pool to process files and accumulate results. The result type R is assumed
+	// and worker poolTransform to process files and accumulate results. The result type R is assumed
 	// to be a monoid, meaning there exists a neutral element for combination, and that
 	// R supports an associative combiner operation.
 	// The result of this collection process, after all reductions, is returned as type R.
@@ -62,10 +66,141 @@ type Crawler[T, R any] interface {
 
 type crawlerImpl[T, R any] struct{}
 
+// New instantiates a new crawler implementation.
 func New[T, R any]() *crawlerImpl[T, R] {
 	return &crawlerImpl[T, R]{}
 }
 
+// combinerGoroutine - description of Collect method combine goroutine
+func (c *crawlerImpl[T, R]) combinerGoroutine(wg *sync.WaitGroup, ctx context.Context, accumOutput <-chan R, finalResult *R, combiner Combiner[R]) {
+	defer wg.Done()
+	for {
+		select {
+		case <-ctx.Done():
+			// interrupted by ctx.Done chan
+			return
+		case accum, ok := <-accumOutput:
+			if !ok {
+				// correct exit
+				return
+			}
+			// Combine the current accumulated value.
+			*finalResult = combiner(*finalResult, accum)
+		}
+	}
+}
+
+// listGoroutine - description of Collect method searcher goroutine
+func (c *crawlerImpl[T, R]) listGoroutine(
+	wgList *sync.WaitGroup,
+	ctx context.Context,
+	conf Configuration,
+	root string,
+	searchOutput chan string,
+	searcher workerpool.Searcher[string],
+	poolTransform workerpool.Pool[string, T],
+) {
+	defer func() {
+		// Ensure searchOutput is closed when done.
+		close(searchOutput)
+		wgList.Done()
+	}()
+	// Use the searcher to list files starting from the root.
+	poolTransform.List(ctx, conf.SearchWorkers, root, searcher)
+}
+
+// searcher - method to get files from one level of filesystem
+func (c *crawlerImpl[T, R]) searcher(
+	once *sync.Once,
+	errorToWrite *error,
+	fileSystem fs.FileSystem,
+	ctx context.Context,
+	searchOutput chan string,
+) func(path string) []string {
+	return func(path string) []string {
+		defer func() {
+			if r := recover(); r != nil {
+				// panic ReadDir
+				panicCheck(r, once, errorToWrite)
+			}
+		}()
+		// read directory
+		entries, err := fileSystem.ReadDir(path)
+		if err != nil {
+			// error ReadDir
+			(*once).Do(func() {
+				writeError(err, errorToWrite)
+			})
+			return nil
+		}
+
+		var children []string
+		for _, entry := range entries {
+			childPath := fileSystem.Join(path, entry.Name())
+			if entry.IsDir() {
+				// append directory
+				children = append(children, childPath)
+			} else {
+				select {
+				case <-ctx.Done():
+					// interrupted by cts.Done chan
+					return nil
+				case searchOutput <- childPath:
+					// send file path to the output chan
+				}
+			}
+		}
+		return children
+	}
+}
+
+// transformer - method to decode files
+func (c *crawlerImpl[T, R]) transformer(
+	once *sync.Once,
+	errorToWrite *error,
+	fileSystem fs.FileSystem,
+	def T,
+) func(path string) T {
+	return func(path string) T {
+		defer func() {
+			// open file panic
+			if r := recover(); r != nil {
+				panicCheck(r, once, errorToWrite)
+			}
+		}()
+
+		// open file
+		file, er := fileSystem.Open(path)
+		if er != nil {
+			// open file error
+			once.Do(func() {
+				writeError(er, errorToWrite)
+			})
+			return def
+		}
+
+		defer func() {
+			// error while closing file
+			if err := file.Close(); err != nil {
+				log.Println("Error while closing file.")
+			}
+		}()
+
+		var result T
+		// decode json
+		if err := json.NewDecoder(file).Decode(&result); err != nil {
+			// json.NewDecoder error
+			once.Do(func() {
+				writeError(err, errorToWrite)
+			})
+			return def
+		}
+
+		return result
+	}
+}
+
+// Collect - implementation of Collect method from Crawler interface
 func (c *crawlerImpl[T, R]) Collect(
 	ctx context.Context,
 	fileSystem fs.FileSystem,
@@ -74,5 +209,66 @@ func (c *crawlerImpl[T, R]) Collect(
 	accumulator workerpool.Accumulator[T, R],
 	combiner Combiner[R],
 ) (R, error) {
-	panic("not implemented")
+	var errorToWrite error
+	once := sync.Once{}
+	poolTransform := workerpool.New[string, T]()
+	searchOutput := make(chan string)
+
+	var defaultValue T
+
+	// wait group for List
+	wgList := sync.WaitGroup{}
+	wgList.Add(1)
+	// search files and place them to the searchOutput chan
+	go c.listGoroutine(&wgList, ctx, conf, root, searchOutput, c.searcher(&once, &errorToWrite, fileSystem, ctx, searchOutput), poolTransform)
+
+	// decode all files from searchOutput and place parsed data to transformOutput chan
+	transformOutput := poolTransform.Transform(ctx, conf.FileWorkers, searchOutput, c.transformer(&once, &errorToWrite, fileSystem, defaultValue))
+
+	// accumulate data
+	poolAccumulate := workerpool.New[T, R]()
+	accumOutput := poolAccumulate.Accumulate(ctx, conf.AccumulatorWorkers, transformOutput, accumulator)
+	var finalResult R
+
+	// wait group for Combiner
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	// combine data to result value
+	go c.combinerGoroutine(&wg, ctx, accumOutput, &finalResult, combiner)
+
+	// wait goroutines List
+	wgList.Wait()
+	// wait goroutine Combiner
+	wg.Wait()
+
+	// return finalResult
+	select {
+	case <-ctx.Done():
+		// interrupted by ctx.Done chan
+		return finalResult, ctx.Err()
+	default:
+		// not interrupted
+	}
+
+	return finalResult, errorToWrite
+
+}
+
+// panicCheck - method to check if there was panic
+func panicCheck(r any, once *sync.Once, errorToWrite *error) {
+	var err error
+	if e, ok := r.(error); ok {
+		err = e
+	} else {
+		err = fmt.Errorf("%v", r)
+	}
+	once.Do(func() {
+		// Write error once using given error pointer.
+		writeError(err, errorToWrite)
+	})
+}
+
+// writeError - method to remember error
+func writeError(err error, errToWrite *error) {
+	*errToWrite = err
 }
--- internal/workerpool/pool.go
+++ internal/workerpool/pool.go
@@ -2,6 +2,7 @@ package workerpool
 
 import (
 	"context"
+	"sync"
 )
 
 // Accumulator is a function type used to aggregate values of type T into a result of type R.
@@ -60,28 +61,171 @@ type Pool[T, R any] interface {
 
 type poolImpl[T, R any] struct{}
 
+// New - Pool constructor
 func New[T, R any]() *poolImpl[T, R] {
 	return &poolImpl[T, R]{}
 }
 
+// accumulateGoroutine - description of Accumulate method goroutine
+// Handles incoming data and accumulates it using the provided accumulator function.
+func (p *poolImpl[T, R]) accumulateGoroutine(wg *sync.WaitGroup, ctx context.Context, input <-chan T, accumulator Accumulator[T, R], result chan R) {
+	var accum R
+	defer wg.Done()
+	defer func() {
+		// Ensures final accumulated result is sent unless context is done.
+		select {
+		case <-ctx.Done():
+			// interrupted by ctx.Done chan
+			return
+		case result <- accum:
+			// Send accumulated result if not interrupted.
+		}
+	}()
+	for {
+		select {
+		case <-ctx.Done():
+			// interrupted by ctx.Done chan
+			return
+		case t, ok := <-input:
+			if !ok {
+				// correct exit
+				return
+			}
+			// accumulator
+			accum = accumulator(t, accum)
+		}
+	}
+}
+
+// Accumulate - implementation of Accumulate method from Pool
+// Spawns multiple goroutines to accumulate values from the input channel.
 func (p *poolImpl[T, R]) Accumulate(
 	ctx context.Context,
 	workers int,
 	input <-chan T,
 	accumulator Accumulator[T, R],
 ) <-chan R {
-	panic("not implemented")
+	// Channel to output accumulated results.
+	result := make(chan R)
+	wg := sync.WaitGroup{}
+	for i := 0; i < workers; i++ {
+		// Start a goroutine for each worker.
+		wg.Add(1)
+		go p.accumulateGoroutine(&wg, ctx, input, accumulator, result)
+	}
+	go func() {
+		defer close(result)
+		wg.Wait()
+	}()
+	return result
 }
 
+// generator - method to move values of type T from children slice to result chan T
+func (p *poolImpl[T, R]) generator(ctx context.Context, children []T, result chan T) {
+	defer close(result)
+	for i := 0; i < len(children); i++ {
+		select {
+		case <-ctx.Done():
+			// interrupted by ctx.Done chan
+			return
+		case result <- children[i]:
+			// Send each child to the result channel.
+		}
+	}
+}
+
+// listGoroutine - description of List method goroutine
+// Processes parent elements to find children using the searcher function.
+func (p *poolImpl[T, R]) listGoroutine(wg *sync.WaitGroup, ctx context.Context, parents <-chan T, searcher Searcher[T], children *[]T) {
+	defer wg.Done()
+	for {
+		select {
+		case <-ctx.Done():
+			// interrupted by ctx.Done chan
+			return
+		case t, ok := <-parents:
+			if !ok {
+				// correct exit
+				return
+			}
+			// searcher
+			*children = searcher(t)
+		}
+	}
+}
+
+// List - implementation of List method from Pool
+// Iteratively explores elements based on searcher function in a tree-like structure.
 func (p *poolImpl[T, R]) List(ctx context.Context, workers int, start T, searcher Searcher[T]) {
-	panic("not implemented")
+	children := []T{start}
+	wg := sync.WaitGroup{}
+	for {
+		parents := make(chan T)
+		for i := 0; i < workers; i++ {
+			// Start a goroutine for each worker.
+			wg.Add(1)
+			go p.listGoroutine(&wg, ctx, parents, searcher, &children)
+		}
+		p.generator(ctx, children, parents)
+		clear(children)
+		wg.Wait()
+		if len(children) == 0 {
+			// Exit if no more children found.
+			break
+		}
+		select {
+		case <-ctx.Done():
+			// interrupted by ctx.Done chan
+			return
+		default:
+			// Continue searching if not interrupted.
+		}
+	}
 }
 
+// transformGoroutine - description of Transform method goroutine
+func (p *poolImpl[T, R]) transformGoroutine(wg *sync.WaitGroup, ctx context.Context, input <-chan T, transformer Transformer[T, R], result chan R) {
+	defer wg.Done()
+	for {
+		select {
+		case <-ctx.Done():
+			// interrupted by ctx.Done chan
+			return
+		case t, ok := <-input:
+			if !ok {
+				// correct exit
+				return
+			}
+			// transformer
+			select {
+			case <-ctx.Done():
+				// interrupted by ctx.Done chan
+				return
+			case result <- transformer(t):
+				// write to result chan
+			}
+		}
+	}
+}
+
+// Transform - implementation of Transform method from Pool
+// Spawns multiple goroutines to transform values from the input channel.
 func (p *poolImpl[T, R]) Transform(
 	ctx context.Context,
 	workers int,
 	input <-chan T,
 	transformer Transformer[T, R],
 ) <-chan R {
-	panic("not implemented")
+	result := make(chan R)
+	wg := sync.WaitGroup{}
+	for i := 0; i < workers; i++ {
+		// Start a goroutine for each worker.
+		wg.Add(1)
+		go p.transformGoroutine(&wg, ctx, input, transformer, result)
+	}
+	go func() {
+		defer close(result)
+		wg.Wait()
+	}()
+	return result
 }
